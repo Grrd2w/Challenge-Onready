56966afe44e5bdc4c731ecb1ce63b4d8b96e99db
Grrd2w <54421642+Grrd2w@users.noreply.github.com>
2021-08-23 00:01:13 -0300

    String a int

--- src/Concecionaria.java
+++ src/Concecionaria.java
@@ -1,16 +1,17 @@
+import java.util.List;
 
 public class Concecionaria  {
 
     public static void main(String[] Args) {
         VhAuto p206 = new VhAuto("Peugeot","206",200000.000,4);
         VhAuto p208 =new VhAuto("Peugeot","206",250000.00,5);
-        VhMoto hTitan =new VhMoto("Honda","Titan",60000.00,"125cc");
-        VhMoto yYbr =new VhMoto("Yamaha","YBR",80500.00,"160cc");
+        VhMoto hTitan =new VhMoto("Honda","Titan",60000.00,125);
+        VhMoto yYbr =new VhMoto("Yamaha","YBR",80500.00,160);
 
         System.out.println("Marca: "+p206.getMarca()+" //"+" Modelo: "+p206.getModelo()+" //"+" Puertas: "+p206.getPuertas()+" //"+" Precio: $"+p206.getPrecio()+"0");
         System.out.println("Marca: "+p208.getMarca()+" //"+" Modelo: "+p208.getModelo()+" //"+" Puertas: "+p208.getPuertas()+" //"+" Precio: $"+p208.getPrecio()+"0");
-        System.out.println("Marca: "+hTitan.getMarca()+" //"+" Modelo: "+hTitan.getModelo()+" //"+" Cilindrada: "+hTitan.getCilindrada()+" //"+" Precio: $"+hTitan.getPrecio()+"0");
-        System.out.println("Marca: "+yYbr.getMarca()+" //"+" Modelo: "+yYbr.getModelo()+" //"+" Cilindrada: "+yYbr.getCilindrada()+" //"+" Precio: $"+yYbr.getPrecio()+"0");
+        System.out.println("Marca: "+hTitan.getMarca()+" //"+" Modelo: "+hTitan.getModelo()+" //"+" Cilindrada: "+hTitan.getCilindrada()+"cc"+" //"+" Precio: $"+hTitan.getPrecio()+"0");
+        System.out.println("Marca: "+yYbr.getMarca()+" //"+" Modelo: "+yYbr.getModelo()+" //"+" Cilindrada: "+yYbr.getCilindrada()+"cc"+" //"+" Precio: $"+yYbr.getPrecio()+"0");
         System.out.println("=====================================");
         System.out.println("Vehiculo mas caro: "+p208.getModelo()+" "+p208.getModelo());
         System.out.println("Vehiculo mas barato: "+hTitan.getMarca()+" "+hTitan.getModelo());
@@ -21,7 +22,5 @@ public class Concecionaria  {
         System.out.println(p206.getMarca() + " "+ p208.getModelo());
         System.out.println(yYbr.getMarca() + " "+ yYbr.getModelo());
         System.out.println(hTitan.getMarca() + " "+ hTitan.getModelo());
-
-
         }
     }
--- src/VhMoto.java
+++ src/VhMoto.java
@@ -1,11 +1,11 @@
 public class VhMoto extends Vehiculo{
-    private String cilindrada;
+    private int cilindrada;
 
-    public VhMoto(String marca, String modelo, double precio, String cilindrada) {
+    public VhMoto(String marca, String modelo, double precio, int cilindrada) {
         super(marca, modelo, precio);
         this.cilindrada = cilindrada;
     }
 
-    public String getCilindrada() {return cilindrada;}
-    public void setCilindrada(String cilindrada) {this.cilindrada = cilindrada;}
+    public int getCilindrada() {return cilindrada;}
+    public void setCilindrada(int cilindrada) {this.cilindrada = cilindrada;}
 }
