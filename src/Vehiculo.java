56c6f313465d24a07dec449680b360d49f551604
Grrd2w <54421642+Grrd2w@users.noreply.github.com>
2021-08-22 23:49:53 -0300

    Update Vehiculo.java

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -3,29 +3,6 @@ public class Vehiculo  {
     private String modelo;
     private double precio;
 
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
 
        public Vehiculo(String marca, String modelo, double precio) {
         this.marca = marca;
@@ -33,4 +10,17 @@ public class Vehiculo  {
         this.precio = precio;
 
     }
+
+
+
+
+
+    public String getMarca() {return marca;}
+    public void setMarca(String marca) {this.marca = marca;}
+
+    public String getModelo() {return modelo;}
+    public void setModelo(String modelo) {this.modelo = modelo;}
+
+    public double getPrecio() {return precio;}
+    public void setPrecio(double precio) {this.precio = precio;}
 }
